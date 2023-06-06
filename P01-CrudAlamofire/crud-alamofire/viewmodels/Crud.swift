35865d311b8ddbc7e8585eab30c8a1d514920292
MacMiniM1 <alonzo.choque@icloud.com>
2023-06-06 12:26:19 -0400

    MacMiniM1 20230606 12:25 P01-CrudAlamofire :: Add Image File to PHP Server

--- P01-CrudAlamofire/crud-alamofire/viewmodels/Crud.swift
+++ P01-CrudAlamofire/crud-alamofire/viewmodels/Crud.swift
@@ -62,7 +62,7 @@ class Crud : ObservableObject {
             "content": content
         ]
         
-        guard let url = URL(string: "http://localhost/proyecto/crud/save.php") else { return }
+        guard let url = URL(string: "https://alonzochoque.net/enews/api/swiftcrud/save.php") else { return }
         
         guard let imgData = imagen.jpegData(compressionQuality: 1.0) else { return }
         let nombreImagen = UUID().uuidString
@@ -70,7 +70,7 @@ class Crud : ObservableObject {
         DispatchQueue.main.async {
             AF.upload(multipartFormData: { MultipartFormData in
                 
-                MultipartFormData.append(imgData, withName: "imagen", fileName: "\(nombreImagen).png", mimeType: "image/png")
+                MultipartFormData.append(imgData, withName: "image", fileName: "\(nombreImagen).png", mimeType: "image/png")
                 
                 for (key, val) in params {
                     MultipartFormData.append((val as AnyObject).data(using: String.Encoding.utf8.rawValue)!, withName: key)
@@ -78,9 +78,34 @@ class Crud : ObservableObject {
                 
             }, to: url, method: .post).uploadProgress { Progress in
                 print(Progress.fractionCompleted * 100)
-            }.response { response in
-                self.mensaje = "Post guardado con exito"
-                self.show = true
+            }.responseData { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data)
+                        let resultJSON = json as! NSDictionary
+                        guard let ok  = resultJSON.value(forKey: "ok")else {return}
+                        guard let res = resultJSON.value(forKey: "msg") else {return}
+                        if ok as! Bool == true {
+                            self.mensaje = res as! String
+                            self.show = true
+                        }else{
+                            self.mensaje = res as! String
+                            self.show = true
+                        }
+                        
+                    }catch let error as NSError{
+                        print ("Error in JSON", error.localizedDescription)
+                        self.mensaje = "Problem to save POST"
+                        self.show = true
+                    }
+                case .failure(let error):
+                    print (error)
+                    self.mensaje = "Problem to save POST"
+                    self.show = true
+                }
+                
+                
             }
         }
         
@@ -88,7 +113,7 @@ class Crud : ObservableObject {
     
     
     func getData(){
-        AF.request("http://localhost/proyecto/crud/select.php")
+        AF.request("https://alonzochoque.net/enews/api/swiftcrud/select.php")
             .responseData { response in
                 switch response.result {
                 case .success(let data):
@@ -115,7 +140,7 @@ class Crud : ObservableObject {
             "nombre_imagen": nombre_imagen
         ]
         
-        guard let url = URL(string: "http://localhost/proyecto/crud/delete.php") else { return }
+        guard let url = URL(string: "https://alonzochoque.net/enews/api/swiftcrud/delete.php") else { return }
         
         DispatchQueue.main.async {
             AF.request(url, method: .post, parameters: params).responseData { response in
